5c283dc51ee14971ca1e31a5b7d0ec07c752f2bb
ishisan98 <e1045760@u.nus.edu>
2023-01-29 15:34:02 +0800

    First commit

---
+++ WebAPI/WebAppApi/src/main/java/com/example/WebAppApi/Model/user.java
@@ -0,0 +1,5 @@
+package com.example.WebAppApi.Model;
+
+public class user {
+    
+}
---
+++ WebAPI/WebAppApi/src/test/java/com/example/WebAppApi/WebAppApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.WebAppApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebAppApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
